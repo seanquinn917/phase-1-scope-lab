95072267f98ceeab1e8370f149d9d55b51bc829e
SeanQuinn <Seanquinn917@gmail.com>
2023-01-05 17:00:49 -0500

    commit tree fc0e08b8c46c54b04d309b392b65df09d340987d

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Write your solution in this file!
+// Write your solution in this file
+var customerName = 'bob';
+
+function upperCaseCustomerName(){
+    return customerName = customerName.toUpperCase(); 
+    
+};
+
+function setBestCustomer(){
+    bestCustomer = 'not bob';
+    return bestCustomer;
+};
+
+function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob'
+    return bestCustomer
+};
+
+const leastFavoriteCustomer = 'value'
+
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = 'newValue'
+    return leastFavoriteCustomer
+}
